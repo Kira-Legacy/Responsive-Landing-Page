abf7c4b82b945913f29aa9fa2f35ee5ea191da26
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-03-19 21:31:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,13 @@ window.addEventListener('scroll', () => {
     if (window.scrollY > 1500) {
         navbar.style.backgroundColor = '#000000';
     }
-    else if (window.scrollY > 1000){
+    else if (window.scrollY > 1000) {
         navbar.style.backgroundColor = '#001a9f';
     }
-   else if (window.scrollY > 500) {
+    else if (window.scrollY > 500) {
         navbar.style.backgroundColor = '#002aff';
     }
     else {
         navbar.style.backgroundColor = '#4060fd';
     }
-})
-
-
-;
+});
